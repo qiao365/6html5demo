9e1d8099e61cd25309f692168aa222cf96de286f
ylkjcjq <691424873@qq.com>
2017-09-07 16:42:04 +0800

    add all

---
+++ 6/products-WB05K85MJ/js/custom.js
@@ -0,0 +1,95 @@
+/*------------------------------------------------------------------
+Project:	HighLand
+Version:	1.2
+Created: 		18/10/2013
+Last change:	26/11/2013
+-------------------------------------------------------------------*/
+
+// Sign in & sing out nav bar demo. To be removed on an operational website
+// ========================================================================
+$('#signin-menu').on('click', function() {
+	$(this).toggleClass('display-none');
+	$("#signup-menu").addClass("display-none");
+	$("#cogs-menu").addClass("display-block");
+	$("#profile-menu").toggleClass("display-block");
+});
+$('#signout-menu').on('click', function() {
+	$("#signup-menu").removeClass("display-none");
+	$("#signin-menu").removeClass("display-none");
+	$("#cogs-menu").removeClass("display-block");
+	$("#profile-menu").removeClass("display-block");
+});
+
+// Search box toggle
+// =================
+$('#search-btn').on('click', function() {
+	$("#search-icon").toggleClass('fa-times margin-2');
+	$("#search-box").toggleClass('display-block animated fadeInUp');
+});
+
+// Smooth scrolling for UI elements page
+// =====================================
+$(document).ready(function(){
+   $('a[href*=#buttons],a[href*=#panels], a[href*=#info-boards], a[href*=#navs], a[href*=#alerts], a[href*=#thumbnails], a[href*=#social], a[href*=#section-header],a[href*=#page-tip], a[href*=#block-header]').bind("click", function(e){
+	  var anchor = $(this);
+	  $('html, body').stop().animate({
+		 scrollTop: $(anchor.attr('href')).offset().top
+	  }, 1000);
+	  e.preventDefault();
+   });
+   return false;
+});
+
+// 404 error page smile
+// ====================
+$('#search-404').on('click', function() {
+	$("#smile").removeClass("fa-meh-o");
+	$("#smile").addClass("fa-smile-o");
+});
+
+// Sign up popovers
+// ================
+$(function(){
+	$('#exampleInputName1').popover();
+});
+
+$(function(){
+	$('#exampleInputUsername1').popover();
+});
+
+$(function(){
+	$('#exampleInputEmail1').popover();
+});
+
+$(function(){
+	$('#exampleInputPassword1').popover();
+});
+
+$(function(){
+	$('#exampleInputPassword2').popover();
+});
+
+// Profile - Status Update 
+// =======================
+
+$('#update-status').on('click', function() {
+	$(".user-status > p").toggleClass("show hidden");
+	$(".user-status > form").toggleClass("hidden show");
+	return false;
+});
+
+$('.user-status > form > button').on('click', function() {
+	$(".user-status > p").toggleClass("show hidden");
+	$(".user-status > form").toggleClass("hidden show");
+	return false;
+});
+
+// Lost password form
+//===================
+
+$('.pwd-lost > .pwd-lost-q > a').on('click', function() {
+	$(".pwd-lost > .pwd-lost-q").toggleClass("show hidden");
+	$(".pwd-lost > .pwd-lost-f").toggleClass("hidden show");
+	return false;
+});
+
